6a5c0ea78e48de5ad861f2995738c1f2d939fe68
Nadia ELHADRI <elhadrinadia219@gmail.com>
2022-04-21 10:20:36 +0200

    add laravel poject

---
+++ ticket/database/migrations/2022_04_19_090420_create_tickets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('status');
+            $table->bigInteger('admin_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('admin_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
---
+++ ticket/database/migrations/2022_04_19_090624_create_commentaire_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commentaire', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('ticket_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('ticket_id')->references('id')->on('films');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commentaire');
+    }
+};
